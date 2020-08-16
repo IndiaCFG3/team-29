fa63345447ed251ab619f71e02adf637efa36104
arunava001OS <arunavasarkar3@gmail.com>
2020-08-16 13:10:07 +0530

    login signup done

---
+++ wastemanagement/app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.conf.urls import url, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('',views.index,name = 'index'),
+    path('signup',views.signup,name='signup'),
+    path('login',views.login_view,name = 'login_view'),
+    path('logout',views.logout_view,name = 'logout'),
+]
--- wastemanagement/app/views.py
+++ wastemanagement/app/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse, HttpResponseRedirect, redirect, reverse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+def index(request):
+    return render(request,'app/index.html',{})
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'app/signup.html', {'form': form})
+
+def login_view(request):
+	message='Log In'
+	if request.method=='POST':
+		_username=request.POST['username']
+		_password=request.POST['password']
+		user=authenticate(username=_username,password=_password)
+		if user is not None:
+			if user.is_active:
+				login(request,user)
+				return redirect('index')
+			else:
+				message='Not Activated'
+		else:
+			message='Invalid Login'
+	context={'message':message}
+	return render(request,'app/login.html',context)
+
+def logout_view(request):
+	logout(request)
+	return redirect('index')
